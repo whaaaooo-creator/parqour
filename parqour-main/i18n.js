5bb2f62962f140e8d0f4e9a680c461b3f9172d3e
whaaaooo-creator <redlyx08@gmail.com>
2026-01-29 14:11:53 +0400

    Add files via upload

---
+++ parqour-main/i18n.js
@@ -0,0 +1,22 @@
+(function(){
+  const DEF='en';
+  const SUPPORTED=['en','ru','az'];
+
+  const urlLang=new URLSearchParams(location.search).get('lang');
+  const stored=localStorage.getItem('lang');
+
+  let lang=urlLang || stored || DEF;
+  if(!SUPPORTED.includes(lang)) lang=DEF;
+
+  localStorage.setItem('lang', lang);
+  document.documentElement.setAttribute('data-lang', lang);
+  window.__lang = lang;
+
+  window.__setLang = function(l){
+    if(!SUPPORTED.includes(l)) l=DEF;
+    localStorage.setItem('lang', l);
+    const u=new URL(location.href);
+    u.searchParams.set('lang', l);
+    location.href=u.toString();
+  };
+})();
